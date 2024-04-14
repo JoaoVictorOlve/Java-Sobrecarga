b79e06d68d869757e27fdcdf4785954db769b77a
João Victor <jvoliveira2005@gmail.com>
2024-04-14 00:32:02 -0300

    Desafio feito

--- Calculadora/src/Calculadora/Operacao.java
+++ Calculadora/src/Calculadora/Operacao.java
@@ -2,52 +2,55 @@ package Calculadora;
 
 public class Operacao {
 	
-	private int nroMinuendo = 0;
-	private int nroSubtraendo = 0;
-	private int nroGenerico1 = 0;
-	private int nroGenerico2 = 0;
-	private int nroFatorial = 0;
+	private int numero1 = 0;
+	private int numero2 = 0;
+
 	public int soma(int numero1, int numero2) {
 		return numero1 + numero2;
 	}
-	public void setMinuendo(int numero) {
-		this.nroMinuendo = numero;
+
+	public void setNumero1(int numero) {
+		this.numero1 = numero;
 	}
-	public void setSubtraendo(int numero) {
-		this.nroSubtraendo = numero;
+
+	public void setNumero2(int numero) {
+		this.numero2 = numero;
 	}
+
 	public int subtracao() {
-		return nroMinuendo -  nroSubtraendo;
+		return numero1 - numero2;
 	}
-	public void setFatorial(int numero) {
-		this.nroFatorial = numero;
-	}
+
 	private int fatorialRecursivo(int numero) {
-		if((numero==1) || (numero==0)) {
+		if((numero == 1) || (numero == 0)) {
 			return 1;
 		} else {
-			return fatorialRecursivo(numero - 1)*numero;
+			return fatorialRecursivo(numero - 1) * numero;
 		}
 	}
+
 	public int getFatorial() {
-		return fatorialRecursivo(nroFatorial);
+		return fatorialRecursivo(numero1);
 	}
+
 	public int multiplicacao() {
-		return this.nroGenerico1 * this.nroGenerico2;
+		return numero1 * numero2;
 	}
-	public void setNumeroGenerico1(int numero) {
-		this.nroGenerico1 = numero;
-	}
-	public void setNumeroGenerico2(int numero) {
-		this.nroGenerico2 = numero;
-	}
+
 	public int divisao() {
-		return this.nroGenerico1 / this.nroGenerico2;
+		return numero1 / numero2;
 	}
+
 	public int soma(int numero1, int numero2, int numero3) {
 		return numero1 + numero2 + numero3;
 	}
+
 	public int soma(String numero1, String numero2) {
 		return Integer.parseInt(numero1) + Integer.parseInt(numero2);
 	}
+	
+	public double exponenciacao(double numero1, double numero2) {
+	    return Math.pow(numero1, numero2);
+	}
+
 }
